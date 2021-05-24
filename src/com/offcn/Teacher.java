83c087700ece5ae938cdc10ffa0395b0e93018a4
liming <451614380@qq.com>
2021-05-24 15:14:44 +0800

    新项目：学生管理系统

---
+++ src/com/offcn/Teacher.java
@@ -0,0 +1,40 @@
+package com.offcn;
+
+public class Teacher {
+    private int tid;
+    private String tname;
+    private int tage;
+
+    public int getTid() {
+        return tid;
+    }
+
+    public void setTid(int tid) {
+        this.tid = tid;
+    }
+
+    public String getTname() {
+        return tname;
+    }
+
+    public void setTname(String tname) {
+        this.tname = tname;
+    }
+
+    public int getTage() {
+        return tage;
+    }
+
+    public void setTage(int tage) {
+        this.tage = tage;
+    }
+
+    public Teacher(int tid, String tname, int tage) {
+        this.tid = tid;
+        this.tname = tname;
+        this.tage = tage;
+    }
+
+    public Teacher() {
+    }
+}
